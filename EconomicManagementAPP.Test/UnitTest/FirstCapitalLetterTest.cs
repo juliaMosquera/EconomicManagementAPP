8ba68dcec94414230c45ab5e5ce32377f0b7535c
juliaMosquera <julia.llarisa@gmail.com>
2022-04-03 14:45:11 -0500

    subiendo cambios

--- EconomicManagementAPP.Test/UnitTest/FirstCapitalLetterTest.cs
+++ EconomicManagementAPP.Test/UnitTest/FirstCapitalLetterTest.cs
@@ -32,30 +32,6 @@ namespace EconomicManagementAPP.Test
             Assert.IsNull(testResult);
         }
 
-        [TestMethod]
-        public void NumberValidateLower_ReturnError()
-        {
-            var numberValidate = new NumberValidate();
-            decimal data = -1;
-
-            var context = new ValidationContext(new { Name = data });
-
-            var testResult = numberValidate.GetValidationResult(data, context);
-            Assert.AreEqual("The field is positive.", testResult?.ErrorMessage);
-        }
-
-        [TestMethod]
-        public void NumberValidationLower_ReturnError()
-        {
-            var numberValidate = new NumberValidate();
-            string data = "a";
-
-            var context = new ValidationContext(new { Name = data });
-
-            var testResult = numberValidate.GetValidationResult(data, context);
-            Assert.AreEqual("The field is numeric type.", testResult?.ErrorMessage);
-        }
-
     }
 
 }
---
+++ EconomicManagementAPP.Test/UnitTest/NumberValidateTest.cs
@@ -0,0 +1,35 @@
+﻿using EconomicManagementAPP.Validations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EconomicManagementAPP.Test
+{
+    [TestClass]
+    public class NumberValidateTest
+    {
+        [TestMethod]
+        public void NumberValidateLower_ReturnError()
+        {
+            var numberValidate = new NumberValidate();
+            decimal data = -1;
+
+            var context = new ValidationContext(new { Name = data });
+
+            var testResult = numberValidate.GetValidationResult(data, context);
+            Assert.AreEqual("The field is positive.", testResult?.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void NumberValidationLower_ReturnError()
+        {
+            var numberValidate = new NumberValidate();
+            string data = "a";
+
+            var context = new ValidationContext(new { Name = data });
+
+            var testResult = numberValidate.GetValidationResult(data, context);
+            Assert.AreEqual("The field is numeric type.", testResult?.ErrorMessage);
+        }
+    }
+}
