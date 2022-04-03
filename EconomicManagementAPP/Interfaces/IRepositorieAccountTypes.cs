d015db081a42dc183fed251f163f137c3e5a0888
juliaMosquera <julia.llarisa@gmail.com>
2022-04-02 19:41:09 -0500

    subiendo cambios

--- EconomicManagementAPP/Interfaces/IRepositorieAccountTypes.cs
+++ EconomicManagementAPP/Interfaces/IRepositorieAccountTypes.cs
@@ -5,8 +5,8 @@ public interface IRepositorieAccountTypes
 {
     Task Create(AccountTypes accountTypes); // Se agrega task por el asincronismo
     Task<bool> Exist(string Name, int UserId);
-    Task<IEnumerable<AccountTypes>> GetAccountsTypes(int UserId = 1);
+    Task<IEnumerable<AccountTypes>> GetAccountsTypes(int UserId);
     Task Modify(AccountTypes accountTypes);
-    Task<AccountTypes> GetAccountById(int id, int userId = 1); // para el modify
+    Task<AccountTypes> GetAccountById(int id, int userId); // para el modify
     Task Delete(int id);
 }
