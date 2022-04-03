54a14868726d74ef4e20902f27731a946a50ba97
juliaMosquera <julia.llarisa@gmail.com>
2022-04-03 00:14:11 -0500

    subiendo cambios

---
+++ EconomicManagementAPP/wwwroot/js/TransactionsForm.js
@@ -0,0 +1,19 @@
+function initializeTransactionsForm(urlGetCategories) {
+    $('#OperationTypesId').change(async function () {
+        const selectedValue = $(this).val();
+
+        const response = await fetch(urlGetCategories, {
+            method: 'POST',
+            body: selectedValue,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const json = await response.json();
+
+        const options = json.map(
+            (category) => `<option value=${category.value}>${category.text}</option>`
+        );
+        $('#CategoryId').html(options);
+    });
+}
